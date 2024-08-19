6e225505ae4597e244567fbf5965382ff0825ff3
TCHATEU NANGKAMG Marc Loic (INTERN) <MTG7@dsone.3ds.com>
2024-08-19 18:11:08 +0200

    WIP

--- foodmine-course/src/app/app.component.ts
+++ foodmine-course/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { StarRatingModule } from 'angular-star-rating';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, StarRatingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
